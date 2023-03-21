79bd87d310c7059610e4f69f771f3648e5e8e5d7
Jeevan Johal <jkjohal@ucdavis.edu>
2023-03-20 22:22:32 -0700

    check local storage for auth token

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,15 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
   return (
