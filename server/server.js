9e823fc7e53ad0e0171594797b659d0eb1012f94
Jeevan Johal <jkjohal@ucdavis.edu>
2023-03-20 22:48:26 -0700

    start apollo server on server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
@@ -26,6 +26,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
 })
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => 
+  console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+})
+};
+
+
+
+startApolloServer(typeDefs, resolvers);
